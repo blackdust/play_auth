bac402191cf134e4f1f365ae058320510d82283d
destinyd <destinyd_war@163.com>
2015-08-06 17:02:59 +0800

    注册后直接登录，且跳转至根目录

--- app/controllers/play_auth/application_controller.rb
+++ app/controllers/play_auth/application_controller.rb
@@ -1,6 +1,7 @@
 module PlayAuth
   class ApplicationController < ActionController::Base
     layout "play_auth/application"
+    include PlayAuth::SessionsHelper
   end
 end
 
--- app/controllers/play_auth/users_controller.rb
+++ app/controllers/play_auth/users_controller.rb
@@ -28,7 +28,8 @@ class PlayAuth::UsersController < PlayAuth::ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to root_path, notice: 'User was successfully created.' }
+        sign_in :user, @user
+        format.html { redirect_to '/', notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
